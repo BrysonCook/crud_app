8046270f2222578eebf7df1a9bb63b6356b329bc
bryson.cook <bryson.cook@my.simpson.edu>
2021-02-25 18:26:33 -0600

    added functionality to add new record in the name_list file, also did some error checking in other files.

--- src/main/webapp/js/name_list.js
+++ src/main/webapp/js/name_list.js
@@ -48,3 +48,76 @@ function getDateFromSQL(sqlDate) {
 
     return resultDate;
 }
+
+// Called when "Add Item" button is clicked
+function showDialogAdd() {
+
+    // Print that we got here
+    console.log("Opening add item dialog");
+
+    // Clear out the values in the form.
+    // Otherwise we'll keep values from when we last
+    // opened or hit edit.
+    // I'm getting it started, you can finish.
+    $('#id').val("");
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
+    // Show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+// There's a button in the form with the ID "addItem"
+// Associate the function showDialogAdd with it.
+let addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
+
+
+function saveChanges() {
+    console.log("Save changes");
+    let firstName = $('#firstName').val();
+    console.log("First name: " + firstName);
+
+    let lastName = $('#lastName').val();
+
+    let phone = $('#phone').val();
+
+    let reg = /^[A-zÀ-ú\s]+$/;
+    let numreg = /^[0-9]{10,11}$/;
+
+    if (reg.test(firstName)) {
+        $('#firstName').removeClass("is-invalid");
+        $('#firstName').addClass("is-valid");
+
+    } else {
+        $('#firstName').removeClass("is-valid");
+        $('#firstName').addClass("is-invalid");
+    }
+
+    if (reg.test(lastName)) {
+        $('#lastName').removeClass("is-invalid");
+        $('#lastName').addClass("is-valid");
+
+    } else {
+        $('#lastName').removeClass("is-valid");
+        $('#lastName').addClass("is-invalid");
+    }
+
+    if (numreg.test(phone)) {
+        $('#phone').removeClass("is-invalid");
+        $('#phone').addClass("is-valid");
+
+    } else {
+        $('#phone').removeClass("is-valid");
+        $('#phone').addClass("is-invalid");
+    }
+}
+
+let saveChangesButton = $('#saveChanges');
+saveChangesButton.on("click", saveChanges);
+
+
